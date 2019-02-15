b7cf3d515ff4e05b222a7912a00dca8a284a432d
Terwer <csyouwei@gmail.com>
2019-02-15 18:46:33 +0800

    Remove java version suffix and update node to 10.15.0 latest LTS version

--- build_system/build_settings.py
+++ build_system/build_settings.py
@@ -7,7 +7,7 @@ used in the build-process (build.py) or the Node.js utility scripts (nodejs.py)
 # Node.js settings
 #-----------------------------------------------------------------------
 # [user-setting] you can change this if you plan to use a different Node.js version for the J2V8 build
-NODE_VERSION_MAJOR, NODE_VERSION_MINOR, NODE_VERSION_PATCH = 7, 4, 0
+NODE_VERSION_MAJOR, NODE_VERSION_MINOR, NODE_VERSION_PATCH = 10, 15, 1
 
 # The Node.js version in the format {major.minor.patch} to be used in other build & utility scripts
 NODE_VERSION = '{}.{}.{}'.format(NODE_VERSION_MAJOR, NODE_VERSION_MINOR, NODE_VERSION_PATCH)
@@ -17,7 +17,7 @@ NODE_VERSION = '{}.{}.{}'.format(NODE_VERSION_MAJOR, NODE_VERSION_MINOR, NODE_VE
 #-----------------------------------------------------------------------
 
 J2V8_VERSION_MAJOR, J2V8_VERSION_MINOR, J2V8_VERSION_PATCH = 4, 8, 3
-J2V8_VERSION_SUFFIX = "-SNAPSHOT"
+J2V8_VERSION_SUFFIX = ""
 
 # The J2V8 version in the format {major.minor.patch} to be used in other build & utility scripts
 J2V8_VERSION = '{}.{}.{}'.format(J2V8_VERSION_MAJOR, J2V8_VERSION_MINOR, J2V8_VERSION_PATCH)
